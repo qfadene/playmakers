eca2ed66a5bba3fd2e4b917ea200eb43f40c7bb5
fadene <fadenework@gmail.com>
2023-07-20 15:50:03 +0200

    adding smallTest

---
+++ smallTest.js
@@ -0,0 +1,41 @@
+/* 
+Print in console the conversion of the given number into a roman numeral 
+and use the Roman DNA to write the missing pairs of DNA for those roman numbers.
+
+For example, for the input 4, return [["I", "V"], ["V","I"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+Roman DNA :
+IV XL CD MY
+
+Roman numerals	Arabic numerals:
+M	1000
+CM	900
+D	500
+CD	400
+C	100
+XC	90
+L	50
+XL	40
+X	10
+IX	9
+V	5
+IV	4
+I	1
+All roman numerals answers should be provided in upper-case.
+
+convertToRomanDNA(2) should return the array [["I", "V"], ["I","V"]].
+convertToRomanDNA(4) should return the array [["I", "V"], ["V","I"]].
+convertToRomanDNA(5) should return the array [["V","I"]].
+convertToRomanDNA(9) should return the array [["I", "V"], ["X","L"]].
+convertToRomanDNA(29) should return the array [["X","L"],["X","L"],["I", "V"],["X","L"]].
+convertToRomanDNA(649) should return the array [["D","C"],["C","D"],["X", "L"],["L","X"],["I", "V"],["X","L"]].
+convertToRomanDNA(2014) should return the array [["M","Y"],["M","Y"],["X", "L"],["I", "V"],["V","I"]].
+*/
+
+function convertToRomanDNA() {
+
+}
+  
+convertToRomanDNA();
